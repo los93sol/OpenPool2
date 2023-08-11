54bf10f1cea25d0b2d4611a85ad479afde7bb0b3
los93sol <adam.t.shortland@gmail.com>
2023-08-11 14:37:02 -0400

    -Make the background not quite as terrible -Allow navigating back to the main menu

--- OpenPool2/Assets/Scenes/SceneLoader.cs
+++ OpenPool2/Assets/Scenes/SceneLoader.cs
@@ -4,5 +4,6 @@ using UnityEngine.SceneManagement;
 public class SceneLoader : MonoBehaviour
 {
     public void LoadGamesMenu() => SceneManager.LoadScene("Games Menu");
+    public void LoadMainMenu() => SceneManager.LoadScene("Main Menu");
     public void LoadSettingsMenu() => SceneManager.LoadScene("Settings Menu");
 }
